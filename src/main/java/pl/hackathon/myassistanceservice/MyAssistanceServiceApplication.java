f2e3d6acaabb0157ac3389659a6398ef5e5c7821
Dawid <majorczyk.dawid@gmail.com>
2019-11-23 13:44:12 +0100

    Initial commit

---
+++ src/main/java/pl/hackathon/myassistanceservice/MyAssistanceServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.hackathon.myassistanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAssistanceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAssistanceServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/hackathon/myassistanceservice/MyAssistanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hackathon.myassistanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAssistanceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
